a8b6a580aebeec51592cf8fcbba55fd8aa330cf6
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2024-12-03 11:32:03 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,25 +1,55 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Math;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
+
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
         Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        int n, i, T, j;
+        System.out.println("Inserisci il numero di cifre da inserire in input: ");
+        n = in.nextInt();
+        int [] U = new int [n];
+        int [] R = new int [n];
+        int [] S = new int [n];
+        int [] M = new int [n];
+        i = 0;
+        while (i<n) {
+            do {
+                System.out.println("Inserisci un numero positivo: ");
+                U[i] = in.nextInt();
+            } while (U[i]<0);
+        }
+        i = 0;
+        while (i<n) {
+            R[i] = Math.random()*11;
+            System.out.println("Numero casuale compreso tra 0 e 10: " + R[i]);
+            i++;
+        }
+        i = 0;
+        while (i<n) {
+            S[i] = (Math.random()*6)+3;
+            System.out.println("Numero casuale compreso tra 3 e 8: " + S[i]);
+            i++;
+        }
+        i = 0;
+        while (i<n) {
+            M[i] = (U[i]+R[i]+S[i])/3;
+            System.out.println("Media = (" + U[i] + " + " + R[i] + " + " + S[i] + ")/3 = " + M[i]);
+            i++;
+        }
+        System.out.println("Inserisci un numero intero: ");
+        T = in.nextInt();
+        i = 0;
+        while (i<n) {
+            j = 0;
+            while (j<n) {
+                if (U[i]+U[j]==T) {
+                    System.out.println(U[i] + " + " + U[j] + " = " + T);
+                }
+                j++;
+            }
+        }
     }
 }
 
